acbc70d7fccd1a2b090ba91d41b7cf6a08d488b5
JaeSeung0529 <vww2021@naver.com>
2024-03-28 15:15:50 +0900

    특정 학번에 해당하는 학생의 정보 수정 및 출력

--- ch04_pjt_01/src/main/java/ch04_pjt_01/ems/MainClass.java
+++ ch04_pjt_01/src/main/java/ch04_pjt_01/ems/MainClass.java
@@ -4,7 +4,9 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 import ch04_pjt_01.ems.member.Student;
 import ch04_pjt_01.ems.member.service.PrintStudentInformationService;
+import ch04_pjt_01.ems.member.service.StudentModifyService;
 import ch04_pjt_01.ems.member.service.StudentRegisterService;
+import ch04_pjt_01.ems.member.service.StudentSelectService;
 import ch04_pjt_01.ems.utils.InitSampleData;
 
 public class MainClass {
@@ -39,10 +41,32 @@ public class MainClass {
 	      psi.printStudentInfo();//학생리스트를 전부 출력
 	      
 	      //학생 등록
-	      
 	      registerService= ctx.getBean("studentRegisterService", StudentRegisterService.class);
 	      registerService.register(new Student("hbs006", "dear","p0006","melissa",26,'W',"Music"));
 	      psi.printStudentInfo();
+	      
+	      //특정 학번에 해당하는 학생 한명만 검색하고 출력
+	      StudentSelectService studentSelectService = 
+	    		  ctx.getBean("studentSelectService",StudentSelectService.class);
+	      
+	      Student selectedstudent = 
+	    		  studentSelectService.select("hbs006");
+
+	      System.out.println("STUDENT START ------------------");
+	      System.out.print("sNum:" + selectedstudent.getsNum() + "\t");
+	      System.out.print("|sId:" + selectedstudent.getsId() + "\t");
+	      System.out.print("|sPw:" + selectedstudent.getsPw() + "\t");
+	      System.out.print("|sName:" + selectedstudent.getsName() + "\t");
+	      System.out.print("|sAge:" + selectedstudent.getsAge() + "\t");
+	      System.out.print("|sGender:" + selectedstudent.getsGender() + "\t");
+	      System.out.println("|sMajor:" + selectedstudent.getsMajor());
+	      System.out.println("END ----------------------------");
+	      
+	      //특정 학번에 해당하는 학생의 정보를 수정하고 출력
+	      
+	      StudentModifyService ModifyService =  
+	      ctx.getBean("studentModifyService",StudentModifyService.class);
+	      
 	}
 	
 
